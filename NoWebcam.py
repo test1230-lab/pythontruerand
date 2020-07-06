b95319bb16bc1016c0b9c274f7e5025a80a074bf
test1230-lab <64430532+test1230-lab@users.noreply.github.com>
2020-07-06 10:25:41 -0400

    Update NoWebcam.py
    added comments

--- NoWebcam.py
+++ NoWebcam.py
@@ -1,7 +1,8 @@
 import hashlib
 import numpy
 import random
-
+#if given the EXACT same image file it will give the same number. 
+#This is why a webcam is recommended. The webcam has noise from the sensor that helps randomize the number, as well as moving objects in the frame.
 x = input("input the file name if it is located in the same dir as the py file or input the image dir if not ")
 BLOCKSIZE = 65536
 hasher = hashlib.sha3_512()
